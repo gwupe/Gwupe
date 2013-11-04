af8b232981b324dd6d4538cec4605ac9099b243f
anandan@anandans.com <anandan@anandans.com>
2013-11-04 21:31:48 +0530

    rating control fix

--- BlitsMeAgent/UI/WPF/Engage/RatingControl.xaml.cs
+++ BlitsMeAgent/UI/WPF/Engage/RatingControl.xaml.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using log4net;
+using System;
 
 namespace BlitsMe.Agent.UI.WPF.Engage
 {
@@ -12,12 +13,18 @@ namespace BlitsMe.Agent.UI.WPF.Engage
     public partial class RatingControl : UserControl
     {
         private int _savedRating = 0;
+        private int _starNo = 0;
         private static readonly ILog Logger = LogManager.GetLogger(typeof(RatingControl));
         private Dictionary<string, int> _ratings;
 
         public RatingControl()
         {
             this.InitializeComponent();
+            poly1.Style = this.FindResource("StarEmpty") as Style;
+            poly2.Style = this.FindResource("StarEmpty") as Style;
+            poly3.Style = this.FindResource("StarEmpty") as Style;
+            poly4.Style = this.FindResource("StarEmpty") as Style;
+            poly5.Style = this.FindResource("StarEmpty") as Style;
         }
 
         private void StarMouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
@@ -27,43 +34,90 @@ namespace BlitsMe.Agent.UI.WPF.Engage
             {
                 if ("Star1".Equals(star.Name))
                 {
-                    RatingCover.Width = 60;
+                    //RatingCover.Width = 60;
+                    _starNo = 1;
                 }
                 else if ("Star2".Equals(star.Name))
                 {
-                    RatingCover.Width = 45;
+                    // RatingCover.Width = 45;
+                    _starNo = 2;
                 }
                 else if ("Star3".Equals(star.Name))
                 {
-                    RatingCover.Width = 30;
+                    //RatingCover.Width = 30;
+                    _starNo = 3;
                 }
                 else if ("Star4".Equals(star.Name))
                 {
-                    RatingCover.Width = 15;
+                    // RatingCover.Width = 15;
+                    _starNo = 4;
                 }
                 else if ("Star5".Equals(star.Name))
                 {
-                    RatingCover.Width = 0;
+                    //RatingCover.Width = 0;
+                    _starNo = 5;
                 }
             }
         }
 
         private void StarClick(object sender, RoutedEventArgs routedEventArgs)
         {
-            _savedRating = (int)(100 - (RatingCover.Width * 100 / 75));
-            MethodInfo methodInfo = DataContext.GetType().GetMethod("SetRating");
-            if(methodInfo != null)
+            //_savedRating = (int)(100 - (RatingCover.Width * 100 / 75));
+            //MethodInfo methodInfo = DataContext.GetType().GetMethod("SetRating");
+            //if(methodInfo != null)
+            //{
+            //    methodInfo.Invoke(DataContext, new object[] {Name, _savedRating});
+            //} else
+            //{
+            //    Logger.Warn("Cannot set rating on server for " + Name + ", no SetRating method on DataContext");
+            //}
+
+            for (int i = 0; i < 5; i++)
             {
-                methodInfo.Invoke(DataContext, new object[] {Name, _savedRating});
-            } else
-            {
-                Logger.Warn("Cannot set rating on server for " + Name + ", no SetRating method on DataContext");
+                switch (_starNo)
+                {
+                    case 1:
+                        poly1.Style = this.FindResource("StarFull") as Style;
+                        poly2.Style = this.FindResource("StarEmpty") as Style;
+                        poly3.Style = this.FindResource("StarEmpty") as Style;
+                        poly4.Style = this.FindResource("StarEmpty") as Style;
+                        poly5.Style = this.FindResource("StarEmpty") as Style;
+                        break;
+                    case 2:
+                        poly1.Style = this.FindResource("StarFull") as Style;
+                        poly2.Style = this.FindResource("StarFull") as Style;
+                        poly3.Style = this.FindResource("StarEmpty") as Style;
+                        poly4.Style = this.FindResource("StarEmpty") as Style;
+                        poly5.Style = this.FindResource("StarEmpty") as Style;
+                        break;
+                    case 3:
+                        poly1.Style = this.FindResource("StarFull") as Style;
+                        poly2.Style = this.FindResource("StarFull") as Style;
+                        poly3.Style = this.FindResource("StarFull") as Style;
+                        poly4.Style = this.FindResource("StarEmpty") as Style;
+                        poly5.Style = this.FindResource("StarEmpty") as Style;
+                        break;
+                    case 4:
+                        poly1.Style = this.FindResource("StarFull") as Style;
+                        poly2.Style = this.FindResource("StarFull") as Style;
+                        poly3.Style = this.FindResource("StarFull") as Style;
+                        poly4.Style = this.FindResource("StarFull") as Style;
+                        poly5.Style = this.FindResource("StarEmpty") as Style;
+                        break;
+                    case 5:
+                        poly1.Style = this.FindResource("StarFull") as Style;
+                        poly2.Style = this.FindResource("StarFull") as Style;
+                        poly3.Style = this.FindResource("StarFull") as Style;
+                        poly4.Style = this.FindResource("StarFull") as Style;
+                        poly5.Style = this.FindResource("StarFull") as Style;
+                        break;
+                }
             }
         }
 
         private void StarMouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            RatingCover.Width = (100 - _savedRating) * 75 / 100;
+            //RatingCover.Width = (100 - _savedRating) * 75 / 100;
         }
     }
 
