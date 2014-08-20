7e5b599d3e26aeb6b5fc5b3727e40146ff664bdf
Darren Hemphill <darren@blits.me>
2014-08-20 23:05:56 +0200

    Preparing for 0.4.23

--- BlitsMeAgent/Properties/AssemblyInfo.cs
+++ BlitsMeAgent/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.22.*")]
+[assembly: AssemblyVersion("0.4.23.*")]
 [assembly: NeutralResourcesLanguageAttribute("en-ZA")]
